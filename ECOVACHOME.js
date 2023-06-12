9440ee10f0a51e8377671bcb403e515316e0c14d
Quangcha <Quangcha@users.noreply.github.com>
2023-06-12 23:04:16 +0700

    Update ECOVACHOME.js

--- ECOVACHOME.js
+++ ECOVACHOME.js
@@ -1,4 +1,2 @@
-var obj = JSON.parse($response.body);
-var replacedJSON = JSON.stringify(obj).replace(/black_list_/g, "");
-var newobj = JSON.parse(replacedJSON);
-$done({body: JSON.stringify(newobj)});
+var body= $response.body.replace(/black_list_/g, "");
+$done({body});
